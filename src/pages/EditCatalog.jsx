97a8a3e070b1320541ca7e1045e605f92cdf7237
Franklin Liu <fezhead@parabolas.xyz>
2023-04-22 12:23:02 -0700

    remove dead code

--- src/pages/EditCatalog.jsx
+++ src/pages/EditCatalog.jsx
@@ -4,17 +4,6 @@ import SearchBox from "../components/Searchbar";
 
 
 
-function BookList() {
-  const [books, setBooks] = useState([]);
-
-  useEffect(() => {
-    localStorage.setItem("books", JSON.stringify(books))
-  }, [books]);
-  return (
-    "nothing here"
-  )
-}
-
 function EditCatalog() {
   return (
     <div>
