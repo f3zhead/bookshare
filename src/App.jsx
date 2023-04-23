8f8ae536ca824be9477ef136c003f2cbed993785
Franklin Liu <fezhead@parabolas.xyz>
2023-04-22 19:01:03 -0700

    hooray

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 // import viteLogo from '/vite.svg'
 import EditCatalog from "./pages/EditCatalog"
 import Catalog from "./pages/Catalog"
+import JoinPeer from "./pages/JoinPeer"
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<EditCatalog />} />
           <Route exact path="/catalog" element={<Catalog />} />
+          <Route exact path="/joinpeer" element={<JoinPeer />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
