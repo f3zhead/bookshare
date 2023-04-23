a67d698ea43dfdc3a24d321c34be4125b3f374ec
Franklin Liu <fezhead@parabolas.xyz>
2023-04-22 17:42:32 -0700

    pain

---
+++ src/components/SearchResult.jsx
@@ -0,0 +1,39 @@
+import { useControllableState, Button, ButtonGroup, Image, Card, Text, CardBody, CardFooter } from '@chakra-ui/react'
+import { useEffect } from 'react';
+
+function SearchResult({ book, catalog, setCatalog }) {
+  const [inCatalog, setInCatalog] = useControllableState({ defaultValue: book in catalog })
+  useEffect(
+    () => {
+      if (inCatalog) {
+        setCatalog(catalog.filter((item) => (item != book)))
+      }
+      else {
+        setCatalog([...catalog, book])
+      }
+    }
+    , [inCatalog]
+  )
+  return (
+    <Card>
+      <CardBody>
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Slaughterhouse-Five_%28first_edition%29_-_Kurt_Vonnegut.jpg/220px-Slaughterhouse-Five_%28first_edition%29_-_Kurt_Vonnegut.jpg" />
+        <Text>{book.title}</Text>
+        <Text>{(book.author_name || []).join(", ")}</Text>
+      </CardBody>
+      <CardFooter>
+        <ButtonGroup spacing={2}>
+          <Button variant="solid"
+            colorScheme={inCatalog ? "red" : "blue"}
+            // smth weird goin on here
+            onclick={() => (setInCatalog(!inCatalog))}>
+            Add to Catalog
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
+  )
+
+}
+export default SearchResult
+
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,6 +1,8 @@
 import { Button, ButtonGroup, Image, Card, VStack, Text, CardHeader, CardBody, CardFooter, SimpleGrid, Box, Input, } from '@chakra-ui/react'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import SearchResult from '../components/SearchResult'
+
 function SearchBox() {
   const [APIData, setAPIData] = useState([]);
   const [query, setQuery] = useState("");
@@ -22,27 +24,16 @@ function SearchBox() {
           type="search"
           onChange={(event) => setQuery(event.target.value)} />
         <SimpleGrid columns={5} spacing={7}>
-          {APIData.map(item => (
-            <Card>
-              <CardBody>
-                <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Slaughterhouse-Five_%28first_edition%29_-_Kurt_Vonnegut.jpg/220px-Slaughterhouse-Five_%28first_edition%29_-_Kurt_Vonnegut.jpg" />
-                <Text>{item.title}</Text>
-                <Text>{(item.author_name || []).join(", ")}</Text>
-              </CardBody>
-              <CardFooter>
-                <ButtonGroup spacing={2}>
-                  <Button variant="solid" colorScheme={item in catalog ? "red" : "green"} onClick={() => setCatalog([...catalog, item])}>
-                    Add to Catalog
-                  </Button>
-                </ButtonGroup>
-              </CardFooter>
-            </Card>
-          )
-          )}
+          {
+            APIData.map(book => (
+              <SearchResult book={book} catalog={catalog} setCatalog={setCatalog} />
+            )
+            )}
         </SimpleGrid>
       </VStack>
     </Box>
   )
 }
+
 
 export default SearchBox;
